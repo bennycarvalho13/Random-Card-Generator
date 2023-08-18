f61ee837b81a0998dc158af632d15bc99284c305
Benny Oliveira <33961839+bennycarvalho13@users.noreply.github.com>
2023-08-18 19:28:31 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+const cardNumbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const cardTypes = [
+  `<h1 class="text-danger">&#9829;</h1>`,
+  `<h1>&#9824;</h1>`,
+  `<h1 class="text-danger">&#9830;</h1>`,
+  `<h1>&#9827;</h1>`
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const generateCard = () => {
+  let number = document.getElementById("number");
+  number.textContent =
+    cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  let randomType = Math.floor(Math.random() * cardTypes.length);
+  document.getElementById("typeOne").innerHTML = cardTypes[randomType];
+  document.getElementById("typeTwo").innerHTML = cardTypes[randomType];
+};
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  everyten();
+};
+document.getElementById("generate").addEventListener("click", function() {
+  generateCard();
+});
+const everyten = () => {
+  generateCard();
+  setTimeout(() => {
+    everyten();
+  }, "10000");
 };
